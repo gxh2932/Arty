1d9cab2727ea3bb283ac988d8c1004b514b5fa29
gxh2932 <ghurt12@gmail.com>
2018-12-30 12:35:38 -0500

    Update light_client.py

--- light_client.py
+++ light_client.py
@@ -60,18 +60,18 @@ def on_message(msg):
             response = str(generate_with_seed(model, seed, MAX_GEN_LEN), encoding='utf-8', errors='backslashreplace').split(':',1)[1]
         else:
             response = str(generate_with_seed(model, seed, MAX_GEN_LEN), encoding='utf-8', errors='backslashreplace')
-        
+
+    elif content.startswith('!arty-pt'):
+        print('hey')
+        mentions = msg.mentions if msg.mentions else (msg.author.mention,)
+        response = ' '.join(i.mention for i in mentions) + ' :PogTard:'
+
     elif client.user.mentioned_in(msg):
         seed = bytes("", encoding='utf-8')
         model.reset_states()
         response = str(generate_with_seed(model, seed, MAX_GEN_LEN), encoding='utf-8', errors='backslashreplace').split(':',1)[1]
     
         response = '{0.author.mention} {1}'.format(msg, response[1:])
-        
-    elif content.startswith('!arty-pt'):
-        
-        mentions = msg.mentions if msg.mentions else (msg.author.mention,)
-        response = ' '.join(i.mention for i in mentions) + ' :PogTard:'
         
     else:
         response = '{0.author.mention} :FailFish:'.format(msg)
